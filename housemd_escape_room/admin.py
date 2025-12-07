d09d32efb89d0e58b91d9a0ca9b45b79590c07ef
Richardbrown85 <richard.s.brown@live.co.uk>
2025-12-07 20:19:12 +0000

    Register Admin

--- housemd_escape_room/admin.py
+++ housemd_escape_room/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
 
 # Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'name', 'email', 'date', 'time', 'number_of_people', 'status', 'created_at']
+    list_filter = ['status', 'date']
+    search_fields = ['order_number', 'name', 'email']
+    date_hierarchy = 'date'
