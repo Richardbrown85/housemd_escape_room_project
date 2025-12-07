449bf91ba35b0012fd920b2a3fccd66d318ecc7f
Richardbrown85 <richard.s.brown@live.co.uk>
2025-12-07 20:03:22 +0000

    Add URLS to urls.py

--- housemd_escape_room_project/urls.py
+++ housemd_escape_room_project/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from housemd_escape_room import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('booking/', views.booking, name='booking'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='housemd_escape_room/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
 ]
+
