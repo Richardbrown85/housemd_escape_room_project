2e8311af52fcf6ffd4013a61f056ae2ff1f646c6
Richardbrown85 <richard.s.brown@live.co.uk>
2026-01-12 19:50:33 +0000

    Add confirmed bookings print to terminal

--- housemd_escape_room_project/settings.py
+++ housemd_escape_room_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +118,7 @@ USE_TZ = True
 # Email Configuration
 
 # For local development - emails print to console
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
