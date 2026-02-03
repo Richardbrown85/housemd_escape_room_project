3663ab59eb892fbed11800c30f2f3d281c368140
Richardbrown85 <richard.s.brown@live.co.uk>
2026-02-03 10:29:57 +0000

    Add booking times to edit_booking.html

--- housemd_escape_room/forms.py
+++ housemd_escape_room/forms.py
@@ -6,17 +6,45 @@ from datetime import date
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
+    TIME_SLOT_CHOICES = [
+        ('', '-- Select a time --'),
+        ('14:00', '2:00 PM'),
+        ('16:00', '4:00 PM'),
+        ('18:00', '6:00 PM'),
+        ('20:00', '8:00 PM'),
+    ]
+
+    time = forms.ChoiceField(
+        choices=TIME_SLOT_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Time'
+    )
+
     class Meta:
         model = Booking
         fields = ['name', 'email', 'phone', 'date', 'time', 'number_of_people']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your full name'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'your.email@example.com'}),
             'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(555) 123-4567'}),
             'number_of_people': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 10}),
         }
+
+    def clean_time(self):
+        """Convert the string choice back to a time object for the model"""
+        from datetime import time as dt_time
+        time_str = self.cleaned_data.get('time')
+        if time_str:
+            hour, minute = map(int, time_str.split(':'))
+            return dt_time(hour, minute)
+        raise ValidationError("Please select a time slot.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Pre-select the current time when editing an existing booking
+        if self.instance.pk and self.instance.time:
+            self.fields['time'].initial = self.instance.time.strftime('%H:%M')
     
     def clean_date(self):
         """Validate that booking date is not in the past"""
