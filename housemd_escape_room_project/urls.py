13c12bfa9d2ee9b329984e33f73b672e01a82d22
Richardbrown85 <richard.s.brown@live.co.uk>
2026-02-14 21:55:52 +0000

    Add URL for password reset email html to be sent instead of Djangos default text

--- housemd_escape_room_project/urls.py
+++ housemd_escape_room_project/urls.py
@@ -32,7 +32,14 @@ urlpatterns = [
     path('booking-confirmation/<str:order_number>/', views.booking_confirmation, name='booking_confirmation'),
     
     # Password Reset URLs
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='housemd_escape_room/password_reset.html'), name='password_reset'),
+    path('password-reset/', 
+     auth_views.PasswordResetView.as_view(
+         template_name='housemd_escape_room/password_reset.html',
+         email_template_name='registration/password_reset_email.txt',  # Plain text fallback
+         html_email_template_name='registration/password_reset_email.html',  # YOUR HTML TEMPLATE
+         subject_template_name='registration/password_reset_subject.txt',  # Optional: custom subject
+     ), 
+     name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='housemd_escape_room/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='housemd_escape_room/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='housemd_escape_room/password_reset_complete.html'), name='password_reset_complete'),
